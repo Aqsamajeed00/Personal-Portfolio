5a4b6130392961f406b90c099d48e3b613fac115
Aqsamajeed00 <aqsamajeedking313@gmail.com>
2024-11-10 03:16:49 +0500

    push code

--- components/Social.jsx
+++ components/Social.jsx
@@ -5,7 +5,7 @@ import {FaGithub, FaLinkedinIn, FaTwitter} from "react-icons/fa";
 const socials = [
     {icon: <FaGithub />, path:"https://github.com/Aqsamajeed00"},
     {icon: <FaLinkedinIn />, path:"https://www.linkedin.com/in/aqsa-majeed-181a69304/"},
-    {icon: <FaTwitter />, path:""},
+    {icon: <FaTwitter />, path:"https://x.com/AqsaShaikh_034?t=S_zkzt0Qs77raAUhskgMQg&s=09"},
 ]
 
 const Social = ({containerStyles, iconStyles}) => {
